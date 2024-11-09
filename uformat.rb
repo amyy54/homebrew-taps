dd15f9e977210d5748271eedbde82a45490c4a1c
Amy C <git@amyy.me>
2024-11-08 21:50:43 -0500

    uformat v0.3.0

--- uformat.rb
+++ uformat.rb
@@ -1,15 +1,17 @@
 class Uformat < Formula
     desc "uformat"
     homepage "https://github.com/amyy54/uformat"
-    url "https://github.com/amyy54/uformat/releases/download/v0.2.0/darwin-universal.tar.gz"
-    sha256 "83f674c6b86691e3ea7e37c6a776346dfb395cae35949ef3621d1893f1f16fa7"
-    version "0.2.0"
+    url "https://github.com/amyy54/uformat/releases/download/v0.3.0/darwin-universal.tar.gz"
+    sha256 "8e1884961853c93ed567afad958e36e53653db5337f897886a172482b00cea5d"
+    version "0.3.0"
 
     def install
         bin.install "./uformat"
+        man.mkpath
+        man1.install "./uformat.1"
     end
 
     test do
-        assert_match /\buformat: v0\.2\.0\b/, shell_output("#{bin}/uformat -version")
+        assert_match /\buformat: v0\.3\.0\b/, shell_output("#{bin}/uformat -version")
     end
 end
